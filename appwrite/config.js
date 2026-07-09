5e8693d75742ae7a8c7ad96ac4fd024dec1fa42b
btoa19studio <btoa19@gmail.com>
2026-07-09 10:30:16 +0700

    Update: Konfigurasi Appwrite Project ID

--- appwrite/config.js
+++ appwrite/config.js
@@ -1,6 +1,6 @@
 const APPWRITE_CONFIG = {
     endpoint: 'https://cloud.appwrite.io/v1',
-    projectId: 'YOUR_PROJECT_ID',  // Ganti dengan Project ID dari Appwrite
+    projectId: '6a4f14c600122aca9f57',  // Ganti dengan Project ID dari Appwrite
 };
 
 export default APPWRITE_CONFIG;
